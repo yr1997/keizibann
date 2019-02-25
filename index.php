08691d0f99f23a40b008ae4172d9b9a50703f33e
yr1997 <45040956+yr1997@users.noreply.github.com>
2019-02-25 14:17:23 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?
 echo 'php掲示板のテストです。<br>';
 $dbinfo = parse_url(getenv('DATABASE_URL'));
-$dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
+$dsn = 'pgsql:host=ec2-107-20-185-27.compute-1.amazonaws.com . $dbinfo['host'] . ';dbname=d22ck7lhd3f5jh' . substr($dbinfo['path'], 1);
 $pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
 var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 phpinfo();

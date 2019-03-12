6e956880ad4bc5e5c9415040e1a9716a2497a6b9
yr1997 <45040956+yr1997@users.noreply.github.com>
2019-03-12 10:45:07 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
-<?
-echo 'php掲示板のテストです。<br>';
-$dbinfo = parse_url(getenv('DATABASE_URL'));
-$dsn = 'pgsql:host=ec2-107-20-185-27.compute-1.amazonaws.com . $dbinfo['host'] . ';dbname=d22ck7lhd3f5jh' . substr($dbinfo['path'], 1);
-$pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
-var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
-phpinfo();
+<?php
+ 
+$title = 'スマブラに参戦しているポケモンはどれ？';
+ 
+$question = array(); //この変数は配列ですよという宣言
+$question = array('リザードン','カメックス','フシギバナ','カビゴン'); //4択の選択肢を設定
+ 
+$answer = $question[0]; //正解の問題を設定
+ 
+shuffle($question); //配列の中身をシャッフル
+ 
 ?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>簡易クイズプログラム</title>
+</head>
+<body>
+ 
+<h2><?php echo $title ?></h2>
+<form method="POST" action="answer.php">
+   <?php foreach($question as $value){ ?>
+   <input type="radio" name="question" value="<?php echo $value; ?>" /> <?php echo $value; ?><br>
+   <?php } ?>
+   <input type="hidden" name="answer" value="<?php echo $answer ?>">
+   <input type="submit" value="回答する">
+</form>
+ 
+</body>
+</html>
